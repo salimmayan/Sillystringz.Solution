e105804bc501637a26adce5ca78ab282d30cd438
Salim Mayan <mailsalim@gmail.com>
2021-06-07 00:09:57 -0700

    add splash page

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,15 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Factory.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly FactoryContext _db;  //FactoryContext is a Model file representing DB
 
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
+        public HomeController(FactoryContext db)
+        {
+            _db = db;
+        }
 
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            ViewBag.EngineerInfo = new List<Engineer>(_db.Engineers.OrderBy(engineer => engineer.EngineerName));
+            ViewBag.MachineInfo = new List<Machine>(_db.Machines.OrderBy(machine => machine.MachineName));
+            return View();
+        }
     }
 }
